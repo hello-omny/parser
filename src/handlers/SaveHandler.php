9e4b6d1664d880e02dd1c3bcd4f69978c3294007
hello.omny <hello.omny@gmail.com>
2018-03-17 20:08:40 +0300

    Added SaveHandler.

---
+++ src/handlers/SaveHandler.php
@@ -0,0 +1,175 @@
+<?php
+
+namespace omny\parser\handlers;
+
+
+use omny\curl\Curl;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Filesystem\Exception\IOExceptionInterface;
+
+class SaveHandler implements HandlerInterface
+{
+    public $url = null;
+    public $storage = 'media';
+    public $name = null;
+
+    protected $baseUploadDir = '/www/site.local/web/upload';
+    protected $baseHttpPath = '/upload';
+    protected $curlTimeout = 300;
+
+    /**
+     * @param array $params
+     * @return mixed|void
+     */
+    public function load(array $params)
+    {
+        $class = new \ReflectionClass($this);
+
+        foreach ($params as $name => $value) {
+            if ($class->hasProperty($name)) {
+                $property = $class->getProperty($name);
+                if ($property->isPublic() && !$property->isStatic()) {
+                    $this->$property = $value;
+                }
+            }
+        }
+    }
+
+    /**
+     * @param bool $validate
+     * @return bool|mixed|string
+     * @throws \Exception
+     */
+    public function run($validate = true)
+    {
+        if ($validate && $this->validate()) {
+            return $this->downloadAndSaveFile($this->url, $this->storage, $this->name);
+        }
+
+        return false;
+    }
+
+    /**
+     * @return bool
+     */
+    public function validate(): bool
+    {
+        if (empty($this->url) || !is_string($this->url)) {
+            return false;
+        }
+
+        if (empty($this->storage) || !is_string($this->storage)) {
+            return false;
+        }
+
+        if (!is_null($this->name) && !is_string($this->name)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * @param string $url
+     * @param string $storage
+     * @param null $name
+     * @return bool|string
+     * @throws \Exception
+     */
+    protected function downloadAndSaveFile(string $url, $storage = 'media', $name = null)
+    {
+        $name = empty($name) ? pathinfo($url, PATHINFO_BASENAME) : $name;
+        $fileExtension = pathinfo($url, PATHINFO_EXTENSION);
+        $generatedFileName = md5($name) . '.' . $fileExtension;
+
+        $relativePath = $this->createStorage($storage, $name);
+        $absolutePath = $this->baseUploadDir . DIRECTORY_SEPARATOR . $relativePath;
+        $httpPath = $this->baseHttpPath . DIRECTORY_SEPARATOR . $relativePath;
+
+        $destinationFile = $absolutePath . DIRECTORY_SEPARATOR . $generatedFileName;
+        $destinationHttpFile = $httpPath . DIRECTORY_SEPARATOR . $generatedFileName;
+
+        if ($this->downloadFile($url, $destinationFile)) {
+            return $destinationHttpFile;
+        }
+
+        return false;
+    }
+
+    /**
+     * @param $storage
+     * @param $salt
+     * @return string
+     * @throws \Exception
+     */
+    protected function createStorage($storage = 'media', $salt)
+    {
+        $hash = md5($salt);
+        $randomDirectory = substr($hash, 0, 7);
+
+        $relativePath = $storage . DIRECTORY_SEPARATOR . $randomDirectory;
+        $absolutePath = $this->baseUploadDir . DIRECTORY_SEPARATOR . $relativePath;
+
+        if (static::createDirectory($absolutePath)) {
+            return $relativePath;
+        };
+
+        return null;
+    }
+
+    /**
+     * @param $path
+     * @param int $mode
+     * @return bool
+     * @throws \Exception
+     */
+    protected function createDirectory($path, $mode = 0775)
+    {
+        $fileSystem = new Filesystem();
+
+        try {
+            $fileSystem->mkdir($path, $mode);
+            return true;
+        } catch (IOExceptionInterface $exception) {
+            throw new \Exception("An error occurred while creating your directory at " . $exception->getPath());
+        }
+    }
+
+    /**
+     * @param string $url
+     * @param string $pathToFile
+     * @return bool
+     */
+    protected function downloadFile(string $url, string $pathToFile)
+    {
+        try {
+            sleep(10);
+
+            $curl = new Curl(['auto_flush' => false]);
+            $destinationFile = @fopen($pathToFile, "w");
+            $curl->setOption(CURLOPT_FILE, $destinationFile);
+            $this->setUpCurl($curl);
+
+            $curl->get($url);
+            fclose($destinationFile);
+            $curl->close();
+
+            return true;
+        } catch (\Exception $e) {
+
+            return false;
+        }
+    }
+
+    /**
+     * @param Curl $curl
+     */
+    private function setUpCurl(Curl $curl)
+    {
+        $curl->setOption(CURLOPT_HEADER, 0);
+        $curl->setOption(CURLOPT_MAXREDIRS, 5);
+        $curl->setOption(CURLOPT_TIMEOUT, $this->curlTimeout);
+        $curl->setOption(CURLOPT_SSL_VERIFYPEER, false);
+    }
+
+}
